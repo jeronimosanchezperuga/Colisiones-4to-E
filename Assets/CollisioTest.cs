61c983aa0752c105955a28f37c633ccf7ae6d533
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-24 16:09:47 -0300

    Initial commit

---
+++ Assets/CollisioTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisioTest : MonoBehaviour
+{
+    void OnCollisionEnter()
+    {
+        Debug.Log("Contacto");
+    }
+    void OnCollisionExit()
+    {
+        Debug.Log("Dejó de hacer Contacto");
+    }
+    void OnCollisionStay()
+    {
+        Debug.Log("Está haciendo Contacto");
+    }
+}
